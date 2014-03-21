4b2d794624d8d8058f9dd630a7368bce4bd44527
Ikuo Matsumura <makiczar@gmail.com>
2014-03-21 23:50:04 +0900

    Update app name

--- src/main/g8/src/main/scala/$main_activity$.scala
+++ src/main/g8/src/main/scala/$main_activity$.scala
@@ -3,11 +3,40 @@ package $package$
 import _root_.android.app.Activity
 import _root_.android.os.Bundle
 
-class $main_activity$ extends Activity with TypedActivity {
+class $main_activity$ extends Activity with TypedActivity { activity =>
   override def onCreate(bundle: Bundle) {
     super.onCreate(bundle)
     setContentView(R.layout.main)
 
-    findView(TR.textview).setText("Hello, this is the $name$ application.")
+    findView(TR.textview).setText("Loading ...")
+    thread.start
+  }
+
+  private val thread: Thread = new Thread(
+    new Runnable {
+      override def run {
+        activity.updateText(testMessage)
+      }
+    }
+  )
+
+  private lazy val testMessage = {
+    // Set dev_id and md5password from ~/work/garapon4s/garapon4s.properties
+    val client = new com.github.ikuo.garapon4s.TvClient("@@dev_id@@")
+    val session =
+      client.newSessionByIp(
+        "192.168.1.11",
+        "matzun",
+        "@@md5password@@"
+      )
+    session.gtvsession
+  }
+
+  private def updateText(text: String) {
+    runOnUiThread(new Runnable {
+      override def run {
+        findView(TR.textview).setText(text)
+      }
+    })
   }
 }
